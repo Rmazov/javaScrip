40cb4cf9961aa7276c124b22e646e19429aad172
unknown <rmazov@hotmail.com>
2022-09-22 10:17:25 -0500

    sC

---
+++ tiposObjetos/ejercicio1.js
@@ -0,0 +1,10 @@
+const mySetFa = new Set()
+
+mySetFa.add("Ricardo")           // Set [ 1 ]
+mySetFa.add("Jesus")           // Set [ 1, 5 ]
+mySetFa.add("Patricia")
+console.log(mySetFa)
+mySetFa.add("Ricardo") 
+console.log(mySetFa)
+mySetFa.add("Javascript") 
+console.log(mySetFa)
